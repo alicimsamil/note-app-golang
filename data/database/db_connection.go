c26db76c0690f8b2b9f3d9c4b9a2010c173c2eb8
alicimsamil <alicimsamil@gmail.com>
2024-07-14 11:39:36 +0300

    Create db connection

---
+++ data/database/db_connection.go
@@ -0,0 +1,41 @@
+package database
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/lib/pq"
+	"log"
+)
+
+const (
+	host       = "localhost"
+	port       = 5432
+	user       = "postgres"
+	password   = "PASSWORD"
+	dbName     = "notes"
+	sslMode    = "disable"
+	driverName = "postgres"
+)
+
+func CreateDBConn() (*sql.DB, error) {
+	connStr := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=%s", host, port, user, password, dbName, sslMode)
+
+	conn, err := sql.Open(driverName, connStr)
+	if err != nil {
+		log.Fatal(err)
+		return nil, err
+	}
+
+	if err = conn.Ping(); err != nil {
+		return nil, err
+	}
+
+	return conn, nil
+}
+
+func CloseDBConn(db *sql.DB) {
+	err := db.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module noteapp
 
 go 1.22
+
+require github.com/lib/pq v1.10.9 // indirect
